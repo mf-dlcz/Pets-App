2a0f121938773329aa5cce357a7b942d66e3a7aa
Maria Delacruz <mar23iaa@gmail.com>
2026-06-12 20:56:00 -0700

    Created two files

--- django/pets_app/views.py
+++ django/pets_app/views.py
@@ -1,11 +1,20 @@
 from django.http import HttpResponse
 from .models import Pet
+from django.shortcuts import render
+
+# New view 
+def pet(request, pet_id):
+    # 1 - prepare application data in a context dict
+    context = {"pet": Pet.objects.filter(id=pet_id).first()}
+    
+    # 2-3-4 - load template, render it and return the HTTP response
+    return render(request, "pets_app/pet.html", context)
+
 
 def listPets(request):
-    
     # Retrieve all the pets from the database.
     all_pets = Pet.objects.all()
-    
+
     # Build a page that displays the properties of each pet.
     response = HttpResponse()
     response.write("<!doctype html>")
@@ -13,7 +22,7 @@ def listPets(request):
     response.write("<head><title>Pets List</title></head>")
     response.write("<body>")
     response.write("<h1>List of Pets</h1>")
-    
+
     for aPet in all_pets:
         response.write("<p>")
         response.write(f"Pet name: {aPet.name} <br>")
@@ -21,9 +30,9 @@ def listPets(request):
         response.write(f"Owner: {aPet.owner} <br>")
         response.write(f"Birthdate: {aPet.birth:%Y-%m-%d} <br>")
         response.write("</p>")
-    
+
     response.write("</body>")
     response.write("</html>")
-    
+
     # Return the response.
     return response
