acfff021b4cf2365f08127a7ce481a856de73882
Maria Delacruz <mar23iaa@gmail.com>
2026-06-15 14:55:24 -0700

    Added a new file and updated content

--- django/pets_app/urls.py
+++ django/pets_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from pets_app import views
+from . import views
 
 urlpatterns = [
     path('pets/', views.listPets, name='petsList'),
+    path("pets/<str:pet_id>", views.pet, name="pet"),
 ]
--- django/pets_app/views.py
+++ django/pets_app/views.py
@@ -6,33 +6,14 @@ from django.shortcuts import render
 def pet(request, pet_id):
     # 1 - prepare application data in a context dict
     context = {"pet": Pet.objects.filter(id=pet_id).first()}
-    
+
     # 2-3-4 - load template, render it and return the HTTP response
     return render(request, "pets_app/pet.html", context)
 
 
 def listPets(request):
-    # Retrieve all the pets from the database.
-    all_pets = Pet.objects.all()
+    context = {
+        'pets': Pet.objects.all()
+    }
 
-    # Build a page that displays the properties of each pet.
-    response = HttpResponse()
-    response.write("<!doctype html>")
-    response.write("<html lang='en'>")
-    response.write("<head><title>Pets List</title></head>")
-    response.write("<body>")
-    response.write("<h1>List of Pets</h1>")
-
-    for aPet in all_pets:
-        response.write("<p>")
-        response.write(f"Pet name: {aPet.name} <br>")
-        response.write(f"Gender: {aPet.gender} <br>")
-        response.write(f"Owner: {aPet.owner} <br>")
-        response.write(f"Birthdate: {aPet.birth:%Y-%m-%d} <br>")
-        response.write("</p>")
-
-    response.write("</body>")
-    response.write("</html>")
-
-    # Return the response.
-    return response
+    return render(request, "pets_app/pets.html", context)
